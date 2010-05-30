d4579ba0eb5d5843899ee7dc924c4f3023598237
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-05-29 22:15:53 -0700

    added charms text and photos

---
+++ Watches_with_Charms_files/Watches_with_Charms.js
@@ -0,0 +1,11 @@
+// Created by iWeb 2.0.4 local-build-20100529
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('Watches_with_Charms_files/Watercolor_iweb_01.png'),IWCreateImage('Watches_with_Charms_files/Watercolor_iweb_02.png'),IWCreateImage('Watches_with_Charms_files/Watercolor_iweb_04.png'),IWCreateImage('Watches_with_Charms_files/Watercolor_iweb_08.png'),IWCreateImage('Watches_with_Charms_files/Watercolor_iweb_16.png'),IWCreateImage('Watches_with_Charms_files/Watercolor_iweb_14.png'),IWCreateImage('Watches_with_Charms_files/Watercolor_iweb_13.png'),IWCreateImage('Watches_with_Charms_files/Watercolor_iweb_05.png')],null,0,0.800000,0.000000,0.000000,0.000000,0.000000,12.000000,12.000000,11.000000,11.000000,13.000000,12.000000,13.000000,12.000000,null,null,null,0.500000),shadow_0:new IWShadow({blurRadius:2,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.300000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Watches_with_Charms_files/Watches_with_CharmsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');adjustLineHeightIfTooBig('id11');adjustFontSizeIfTooBig('id11');adjustLineHeightIfTooBig('id12');adjustFontSizeIfTooBig('id12');adjustLineHeightIfTooBig('id13');adjustFontSizeIfTooBig('id13');adjustLineHeightIfTooBig('id14');adjustFontSizeIfTooBig('id14');fixAllIEPNGs('Media/transparent.gif');Widget.onload();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
