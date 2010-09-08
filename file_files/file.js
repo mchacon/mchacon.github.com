eeedf8094838b187b82fa175edd0f6cf07db079a
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-09-07 20:35:37 -0700

    old updates

---
+++ file_files/file.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20100907
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('file_files/fileMoz.css')
+fixAllIEPNGs('Media/transparent.gif');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
